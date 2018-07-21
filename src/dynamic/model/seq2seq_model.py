055cfada66c54dfc0b77bd6d9b29b7f24edbfdc8
jinxin0924 <jinxin900924@gmail.com>
2018-07-21 15:31:54 +0800

    update

--- src/dynamic/model/seq2seq_model.py
+++ src/dynamic/model/seq2seq_model.py
@@ -22,206 +22,283 @@ from __future__ import print_function
 import tensorflow as tf
 
 
-# from tensorflow.models.rnn.translate import data_utils
-# from tensorflow.nn import rnn, rnn_cell
-
-
 class Seq2SeqModel(object):
-  """Sequence-to-sequence model with attention and for multiple buckets.
-  This class implements a multi-layer recurrent neural network as encoder,
-  and an attention-based decoder. This is the same as the model described in
-  this paper: http://arxiv.org/abs/1412.7449 - please look there for details,
-  or into the seq2seq library for complete model implementation.
-  This class also allows to use GRU cells in addition to LSTM cells, and
-  sampled softmax to handle large output vocabulary size. A single-layer
-  version of this model, but with bi-directional encoder, was presented in
-    http://arxiv.org/abs/1409.0473
-  and sampled softmax is described in Section 3 of the following paper.
-    http://arxiv.org/abs/1412.2007
-  """
-
-  def __init__(self, encoder_masks, encoder_inputs_tensor,
+  def __init__(self, encoder_inputs_tensor,
                decoder_inputs,
-               target_weights,
+               sequence_length,
+               source_sequence_length,
                target_vocab_size,
-               buckets,
                target_embedding_size,
+               target_sequence_length,
                attn_num_layers,
                attn_num_hidden,
                mode,
                use_gru,
-               beam_width):
-    """Create the model.
-
-    Args:
-      source_vocab_size: size of the source vocabulary.
-      target_vocab_size: size of the target vocabulary.
-      buckets: a list of pairs (I, O), where I specifies maximum input length
-        that will be processed in that bucket, and O specifies maximum output
-        length. Training instances that have inputs longer than I or outputs
-        longer than O will be pushed to the next bucket and padded accordingly.
-        We assume that the list is sorted, e.g., [(2, 4), (8, 16)].
-      size: number of units in each layer of the model.
-      num_layers: number of layers in the model.
-      max_gradient_norm: gradients will be clipped to maximally this norm.
-      learning_rate: learning rate to start with.
-      learning_rate_decay_factor: decay learning rate by this much when needed.
-      use_lstm: if true, we use LSTM cells instead of GRU cells.
-      num_samples: number of samples for sampled softmax.
-      forward_only: if set, we do not construct the backward pass in the model.
-    """
+               beam_width,
+               time_major,
+               attention_option):
     self.encoder_inputs_tensor = encoder_inputs_tensor
     self.decoder_inputs = decoder_inputs
-    self.target_weights = target_weights
     self.target_vocab_size = target_vocab_size
-    self.buckets = buckets
-    self.encoder_masks = encoder_masks
     self.mode = mode
+    self.sequence_length = sequence_length
+    self.source_sequence_length = source_sequence_length
+    self.beam_width = beam_width
+    self.time_major = time_major
 
-    # Create the internal multi-layer cell for our RNN.
-    def _create_cell(attn_num_hidden, attn_num_layers, use_gru=False):
-      if attn_num_layers == 1:
-        if use_gru:
-          return tf.contrib.rnn.GRUCell(attn_num_hidden)
-        else:
-          return tf.contrib.rnn.BasicLSTMCell(attn_num_hidden, forget_bias=0.0,
-                                              state_is_tuple=False)
-      else:
-        cell_list = []
-        for i in range(attn_num_layers):
-          if use_gru:
-            cell_list.append(tf.contrib.rnn.GRUCell(attn_num_hidden))
-          else:
-            cell_list.append(
-              tf.contrib.rnn.BasicLSTMCell(attn_num_hidden, forget_bias=0.0,
-                                           state_is_tuple=False))
-        return tf.contrib.rnn.MultiRNNCell(cell_list, state_is_tuple=False)
+    dtype = tf.float32
+    self.output_layer = tf.layers.Dense(
+      target_vocab_size, use_bias=False, name="output_projection")
 
-    with tf.variable_scope("dynamic_seq2seq", dtype=dtype):
+    self.embedding_decoder = tf.get_variable(
+      'embedding', [target_vocab_size, target_embedding_size], dtype)
+
+    with tf.variable_scope("dynamic_seq2seq"):
       # build encoder
       with tf.variable_scope("encoder"):
-        fw_cell = _create_cell(num_bi_layers, num_bi_residual_layers, use_gru)
-        bw_cell = _create_cell(num_bi_layers, num_bi_residual_layers, use_gru)
-
-        bi_outputs, bi_state = tf.nn.bidirectional_dynamic_rnn(
-          fw_cell,
-          bw_cell,
-          inputs,
-          dtype=dtype,
-          sequence_length=sequence_length,
-          time_major=self.time_major,
-          swap_memory=True)
-
-        encoder_outputs, bi_encoder_state = tf.concat(bi_outputs, -1), bi_state
-        if num_bi_layers == 1:
-          encoder_state = bi_encoder_state
-        else:
-          # alternatively concat forward and backward states
-          encoder_state = []
-          for layer_id in range(num_bi_layers):
-            encoder_state.append(bi_encoder_state[0][layer_id])  # forward
-            encoder_state.append(bi_encoder_state[1][layer_id])  # backward
-          encoder_state = tuple(encoder_state)
+        encoder_outputs, encoder_state = self._build_encoder(
+          encoder_inputs_tensor,
+          sequence_length,
+          attn_num_hidden,
+          attn_num_layers,
+          use_gru)
 
       ## Decoder.
-      with tf.variable_scope("decoder") as decoder_scope:
-        self.embedding_decoder = tf.get_variable(
-          'embedding', [target_vocab_size, target_embedding_size], dtype)
+      with tf.variable_scope("decoder"):
+        logits, sample_id, final_context_state = self._build_decoder(
+          target_input, target_sequence_length,
+          attention_option,
+          encoder_outputs, encoder_state, attn_num_hidden,
+          attn_num_layers, use_gru)
 
-        decoder_cell = _create_cell(attn_num_hidden, attn_num_layers, use_gru)
+      if self.mode != tf.contrib.learn.ModeKeys.INFER:
+        loss = self._compute_loss(target_input, logits, target_sequence_length)
+      else:
+        loss = None
+    self.logits = logits
+    self.loss = loss
+    self.final_context_state = final_context_state
+    self.sample_id = sample_id
 
-        if self.mode == tf.contrib.learn.ModeKeys.INFER and beam_width > 0:
-          decoder_initial_state = tf.contrib.seq2seq.tile_batch(
-            encoder_state, multiplier=beam_width)
+  # Create the internal multi-layer cell for our RNN.
+  def _create_cell(self, attn_num_hidden, attn_num_layers, use_gru=False):
+    if attn_num_layers == 1:
+      if use_gru:
+        return tf.contrib.rnn.GRUCell(attn_num_hidden)
+      else:
+        return tf.contrib.rnn.BasicLSTMCell(attn_num_hidden, forget_bias=0.0,
+                                            state_is_tuple=False)
+    else:
+      cell_list = []
+      for i in range(attn_num_layers):
+        if use_gru:
+          cell_list.append(tf.contrib.rnn.GRUCell(attn_num_hidden))
         else:
-          decoder_initial_state = encoder_state
+          cell_list.append(
+            tf.contrib.rnn.BasicLSTMCell(attn_num_hidden, forget_bias=0.0,
+                                         state_is_tuple=False))
+      return tf.contrib.rnn.MultiRNNCell(cell_list, state_is_tuple=False)
 
-        ## Train or eval
-        if self.mode != tf.contrib.learn.ModeKeys.INFER:
-          # decoder_emp_inp: [max_time, batch_size, num_units]
-          target_input = iterator.target_input
-          if self.time_major:
-            target_input = tf.transpose(target_input)
+  def _build_encoder(self, inputs, sequence_length, attn_num_hidden,
+                     attn_num_layers, use_gru):
+    num_bi_layers = attn_num_hidden // 2
+    fw_cell = self.create_cell(num_bi_layers, attn_num_layers, use_gru)
+    bw_cell = self.create_cell(num_bi_layers, attn_num_layers, use_gru)
 
-          decoder_emb_inp = tf.nn.embedding_lookup(
-            self.embedding_decoder, target_input)
+    bi_outputs, bi_state = tf.nn.bidirectional_dynamic_rnn(
+      fw_cell,
+      bw_cell,
+      inputs,
+      dtype=dtype,
+      sequence_length=sequence_length,
+      time_major=self.time_major,
+      swap_memory=True)
 
-          # Helper
-          helper = tf.contrib.seq2seq.TrainingHelper(
-            decoder_emb_inp, iterator.target_sequence_length,
-            time_major=self.time_major)
+    encoder_outputs, bi_encoder_state = tf.concat(bi_outputs, -1), bi_state
+    if num_bi_layers == 1:
+      encoder_state = bi_encoder_state
+    else:
+      # alternatively concat forward and backward states
+      encoder_state = []
+      for layer_id in range(num_bi_layers):
+        encoder_state.append(bi_encoder_state[0][layer_id])  # forward
+        encoder_state.append(bi_encoder_state[1][layer_id])  # backward
+      encoder_state = tuple(encoder_state)
+    return encoder_outputs, encoder_state
 
-          # Decoder
-          my_decoder = tf.contrib.seq2seq.BasicDecoder(
-            decoder_cell,
-            helper,
-            decoder_initial_state, )
+  def _attention_mechanism_fn(self, attention_option, num_units, memory,
+                              source_sequence_length):
+    """Create attention mechanism based on the attention_option."""
+    # Mechanism
+    if attention_option == "luong":
+      attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+        num_units, memory, memory_sequence_length=source_sequence_length)
+    elif attention_option == "scaled_luong":
+      attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+        num_units,
+        memory,
+        memory_sequence_length=source_sequence_length,
+        scale=True)
+    elif attention_option == "bahdanau":
+      attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+        num_units, memory, memory_sequence_length=source_sequence_length)
+    elif attention_option == "normed_bahdanau":
+      attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+        num_units,
+        memory,
+        memory_sequence_length=source_sequence_length,
+        normalize=True)
+    else:
+      raise ValueError("Unknown attention option %s" % attention_option)
 
-          # Dynamic decoding
-          outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
-            my_decoder,
-            output_time_major=self.time_major,
-            swap_memory=True,
-            scope=decoder_scope)
+    return attention_mechanism
 
-          sample_id = outputs.sample_id
+  def _build_decoder(self, target_input, target_sequence_length,
+                     attention_option, encoder_outputs,
+                     encoder_state,
+                     attn_num_hidden,
+                     attn_num_layers, use_gru, sampling_temperature=0.0,
+                     maximum_iterations=20):
+    tgt_sos_id = tf.cast(0, tf.int32)
+    tgt_eos_id = tf.cast(1, tf.int32)
+    if self.time_major:
+      memory = tf.transpose(encoder_outputs, [1, 0, 2])
+    else:
+      memory = encoder_outputs
 
-          # Note: there's a subtle difference here between train and inference.
-          # We could have set output_layer when create my_decoder
-          #   and shared more code between train and inference.
-          # We chose to apply the output_layer to all timesteps for speed:
-          #   10% improvements for small models & 20% for larger ones.
-          # If memory is a concern, we should apply output_layer per timestep.
-          logits = self.output_layer(outputs.rnn_output)
+    if self.mode == tf.contrib.learn.ModeKeys.INFER and self.beam_width > 0:
+      memory = tf.contrib.seq2seq.tile_batch(
+        memory, multiplier=self.beam_width)
+      source_sequence_length = tf.contrib.seq2seq.tile_batch(
+        self.source_sequence_length, multiplier=beam_width)
+      encoder_state = tf.contrib.seq2seq.tile_batch(
+        encoder_state, multiplier=self.beam_width)
+      batch_size = self.batch_size * self.beam_width
+    else:
+      batch_size = self.batch_size
 
-        ## Inference
+    attention_mechanism = self._attention_mechanism_fn(
+      attention_option, attn_num_hidden, memory, self.source_sequence_length)
+
+    decoder_cell = self._create_cell(attn_num_hidden, attn_num_layers, use_gru)
+
+    alignment_history = (self.mode == tf.contrib.learn.ModeKeys.INFER and
+                         self.beam_width == 0)
+
+    cell = tf.contrib.seq2seq.AttentionWrapper(
+      decoder_cell,
+      attention_mechanism,
+      attention_layer_size=attn_num_hidden,
+      alignment_history=alignment_history,
+      output_attention=True,
+      name="attention")
+
+    if self.mode == tf.contrib.learn.ModeKeys.INFER and self.beam_width > 0:
+      decoder_initial_state = tf.contrib.seq2seq.tile_batch(
+        encoder_state, multiplier=beam_width)
+    else:
+      decoder_initial_state = cell.clone(cell_state=encoder_state)
+
+    ## Train or eval
+    if self.mode != tf.contrib.learn.ModeKeys.INFER:
+      # decoder_emp_inp: [max_time, batch_size, num_units]
+      if self.time_major:
+        target_input = tf.transpose(target_input)
+
+      decoder_emb_inp = tf.nn.embedding_lookup(
+        self.embedding_decoder, target_input)
+
+      # Helper
+      helper = tf.contrib.seq2seq.TrainingHelper(
+        decoder_emb_inp, target_sequence_length,
+        time_major=self.time_major)
+
+      # Decoder
+      my_decoder = tf.contrib.seq2seq.BasicDecoder(
+        decoder_cell,
+        helper,
+        decoder_initial_state, )
+
+      # Dynamic decoding
+      outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
+        my_decoder,
+        output_time_major=self.time_major,
+        swap_memory=True,
+        scope=decoder_scope)
+
+      sample_id = outputs.sample_id
+      logits = self.output_layer(outputs.rnn_output)
+
+    ## Inference
+    else:
+      length_penalty_weight = 0.0
+      start_tokens = tf.fill([self.batch_size], tgt_sos_id)
+      end_token = tgt_eos_id
+
+      if self.beam_width > 0:
+        my_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
+          cell=cell,
+          embedding=self.embedding_decoder,
+          start_tokens=start_tokens,
+          end_token=end_token,
+          initial_state=decoder_initial_state,
+          beam_width=beam_width,
+          output_layer=self.output_layer,
+          length_penalty_weight=length_penalty_weight)
+      else:
+        # Helper
+        sampling_temperature = sampling_temperature
+        if sampling_temperature > 0.0:
+          helper = tf.contrib.seq2seq.SampleEmbeddingHelper(
+            self.embedding_decoder, start_tokens, end_token,
+            softmax_temperature=sampling_temperature)
         else:
-          length_penalty_weight = hparams.length_penalty_weight
-          start_tokens = tf.fill([self.batch_size], tgt_sos_id)
-          end_token = tgt_eos_id
+          helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
+            self.embedding_decoder, start_tokens, end_token)
 
-          if beam_width > 0:
-            my_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
-              cell=cell,
-              embedding=self.embedding_decoder,
-              start_tokens=start_tokens,
-              end_token=end_token,
-              initial_state=decoder_initial_state,
-              beam_width=beam_width,
-              output_layer=self.output_layer,
-              length_penalty_weight=length_penalty_weight)
-          else:
-            # Helper
-            sampling_temperature = hparams.sampling_temperature
-            if sampling_temperature > 0.0:
-              helper = tf.contrib.seq2seq.SampleEmbeddingHelper(
-                self.embedding_decoder, start_tokens, end_token,
-                softmax_temperature=sampling_temperature,
-                seed=hparams.random_seed)
-            else:
-              helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
-                self.embedding_decoder, start_tokens, end_token)
+        # Decoder
+        my_decoder = tf.contrib.seq2seq.BasicDecoder(
+          cell,
+          helper,
+          decoder_initial_state,
+          output_layer=self.output_layer  # applied per timestep
 
-            # Decoder
-            my_decoder = tf.contrib.seq2seq.BasicDecoder(
-              cell,
-              helper,
-              decoder_initial_state,
-              output_layer=self.output_layer  # applied per timestep
+        )
 
-            )
+    # Dynamic decoding
+    outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
+      my_decoder,
+      maximum_iterations=maximum_iterations,
+      output_time_major=self.time_major,
+      swap_memory=True,
+      scope=decoder_scope)
 
-        # Dynamic decoding
-        outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
-          my_decoder,
-          maximum_iterations=maximum_iterations,
-          output_time_major=self.time_major,
-          swap_memory=True,
-          scope=decoder_scope)
+    if self.beam_width > 0:
+      logits = tf.no_op()
+      sample_id = outputs.predicted_ids
+    else:
+      logits = outputs.rnn_output
+      sample_id = outputs.sample_id
+    return logits, sample_id, final_context_state
 
-        if beam_width > 0:
-          logits = tf.no_op()
-          sample_id = outputs.predicted_ids
-        else:
-          logits = outputs.rnn_output
-          sample_id = outputs.sample_id
+
+def _compute_loss(self, target_output, logits, target_sequence_length):
+  """Compute optimization loss."""
+
+  def get_max_time(self, tensor):
+    time_axis = 0 if self.time_major else 1
+    return tensor.shape[time_axis].value or tf.shape(tensor)[time_axis]
+
+  if self.time_major:
+    target_output = tf.transpose(target_output)
+  max_time = get_max_time(target_output)
+  crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    labels=target_output, logits=logits)
+  target_weights = tf.sequence_mask(target_sequence_length, max_time,
+                                    dtype=logits.dtype)
+  if self.time_major:
+    target_weights = tf.transpose(target_weights)
+
+  loss = tf.reduce_sum(
+    crossent * target_weights) / tf.to_float(self.batch_size)
+  return loss
